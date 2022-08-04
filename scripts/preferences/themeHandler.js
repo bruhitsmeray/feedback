7bdc232470fe985b0ba176564bba8c52bc261267
Morar Andrei <whiteex@icloud.com>
2022-08-04 10:57:38 +0300

    August 4th, 2022
    - Changed the way themes are handled
    - The user's preferred theme is now saved for future visits

---
+++ scripts/preferences/themeHandler.js
@@ -0,0 +1,52 @@
+const checkbox = document.getElementById("checkbox");
+const CurrentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : null;
+
+window.onload = Startup();
+
+function Startup() {
+  if (CurrentTheme) {
+    document.documentElement.setAttribute("data-theme", CurrentTheme);
+
+    if (CurrentTheme === "dark") {
+      checkbox.checked = true;
+    } else {
+      checkbox.checked = false;
+    }
+  } else {
+    GetSysColorMode();
+  }
+}
+
+checkbox.addEventListener("change", () => {
+  IfChecked();
+});
+
+function GetSysColorMode() {
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+    checkbox.checked = true;
+  } else if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: light)").matches
+  ) {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+    checkbox.checked = false;
+  }
+}
+
+function IfChecked() {
+  if (checkbox.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+}
